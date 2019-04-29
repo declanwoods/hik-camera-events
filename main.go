a2e8ffe35d32dbd5bf0d8a946df4c8fe4011eb59
Declan Woods <docdecdoc@hotmail.com>
2019-04-29 23:49:35 +0800

    First

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+import "net/http"
+import "bufio"
+import "strings"
+
+func main() {
+  client := &http.Client{}
+  req, _ := http.NewRequest("GET", "http://192.168.0.131:80/ISAPI/Event/notification/alertStream",nil)
+  req.SetBasicAuth("admin","**********")
+  res, _ := client.Do(req)
+
+  if resp != nil {
+    reader := bufio.NewReader(res.Body)
+
+    etype := ""
+    datetime := ""
+    state := ""
+
+    for {
+      line, _ := reader.ReadBytes("\n")
+      if string(line) == "" {
+        break
+      }
+
+      s := strings.Split(string(line), "<eventType>")
+      if len(s) > 1 {
+        x := strings.Split(s[1], "</eventType>")
+        etype = strings.TrimSpace(x[0])
+      }  
+      s = strings.Split(string(line), "<dateTime>")
+      if len(s) > 1 {
+        x := strings.Split(s[1], "</dateTime>")
+        datetime = strings.TrimSpace(x[0])
+      }  
+      s = strings.Split(string(line), "<eventState>")
+      if len(s) > 1 {
+        x := strings.Split(s[1], "</eventState>")
+        state = strings.TrimSpace(x[0])
+      }        
+
+      if strings.Index(string(line), "--boundary") > -1 {
+        if etype != "d" && etype != "" {
+          fmt.Println("New event,", etype, datetime, state)
+        }
+      }
+    }
+  }
+}
